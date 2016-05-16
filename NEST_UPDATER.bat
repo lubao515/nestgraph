f622caa63d238b49e5ffeca1fd7d9630b2458d77
DeFlanko <DeFlanko@users.noreply.github.com>
2016-05-15 21:00:25 -0700

    Add files via upload

---
+++ NEST_UPDATER.bat
@@ -0,0 +1,2 @@
+php C:\inetpub\nest\insert.php
+php C:\inetpub\nest\insertEnergy.php
--- inc/config.php
+++ inc/config.php
@@ -2,11 +2,11 @@
 
 $config = array('db_ip' => '127.0.0.1', 
 		'db_user' => 'nest_admin',
-		'db_pass' => 'choose_a_db_password',
+		'db_pass' => 'December31st',
 		'db_name' => 'nest',
-		'nest_user' => 'your_nest_username',
-		'nest_pass' => 'your_nest_password',
-		'local_tz' => 'America/New_York' // see http://php.net/manual/en/timezones.php
+		'nest_user' => 'DeFlankoDesigns@gmail.com',
+		'nest_pass' => 'qwerty789',
+		'local_tz' => 'America/Los_Angeles' // see http://php.net/manual/en/timezones.php
 		);
 
 ?>
