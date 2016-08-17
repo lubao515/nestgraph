c13af79fc0f2fd54145cafa5cba31cbc7b18b108
李 博 <lubao515@MacBook-Pro-4.local>
2016-08-16 23:26:20 -0400

    add config.php

--- inc/config.php
+++ inc/config.php
@@ -4,9 +4,8 @@ $config = array('db_ip' => '127.0.0.1',
 		'db_user' => 'nest_admin',
 		'db_pass' => 'Your Desired DB Password for db_user',
 		'db_name' => 'nest',
-		'nest_user' => 'Your Nest.com Username',
-		'nest_pass' => 'Your Nest.com Password',
-		'local_tz' => 'America/Los_Angeles' // see http://php.net/manual/en/timezones.php
+		'nest_token' => 'YOUR_ACCESS_TOKEN',
+		'local_tz' => 'America/New_York' // see http://php.net/manual/en/timezones.php
 		);
 
 ?>
